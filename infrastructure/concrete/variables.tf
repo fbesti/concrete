a8a7a0497eb13ff972406ba10befdfb9a150bcd7
Freyr Finnbogason <freyrbesti@gmail.com>
2025-09-21 10:39:58 +0000

    fixing workflow and tf files

--- infrastructure/concrete/variables.tf
+++ infrastructure/concrete/variables.tf
@@ -11,7 +11,7 @@ variable "resource_group_name" {
 }
 
 variable "storage_account_name" {
-    description = "The name of the resource group."
+    description = "The name of the storage account."
   type        = string
-  default = "stconcrete8s9kwks"
+  default = "stconcrete8s9kskowks"
 }
