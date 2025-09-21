8de88521cbd08d3585b9f11815bd9bd1664ee129
Freyr Finnbogason <freyrbesti@gmail.com>
2025-09-21 11:45:24 +0000

    fix workflows

--- infrastructure/concrete/variables.tf
+++ infrastructure/concrete/variables.tf
@@ -13,7 +13,7 @@ variable "resource_group_name" {
 variable "storage_account_name" {
   description = "The name of the storage account."
   type        = string
-  default     = "stconcrete8s9kskowks"
+  default     = "stconcrete8s9ksjs7owks"
   validation {
     condition     = can(regex("^[a-z0-9]{3,24}$", var.storage_account_name))
     error_message = "Storage account name must be 3-24 characters, lowercase letters and numbers only."
