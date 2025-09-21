35e98a9dd75d76ba2b26f9434da611c2b604c693
Freyr Finnbogason <freyrbesti@gmail.com>
2025-09-21 12:07:08 +0000

    fix failure to apply

--- infrastructure/concrete/main.tf
+++ infrastructure/concrete/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "resource_group" {
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -17,7 +17,6 @@ resource "azurerm_storage_account" "storage_account" {
   tags = {
     environment = "mvp"
   }
-  depends_on = [ azurerm_resource_group.resource_group ]
 }
 resource "azurerm_storage_container" "documents" {
   name                  = "documents"
