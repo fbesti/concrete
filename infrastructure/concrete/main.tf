34fb7a9653d4d87989583267961a613b56eb9ba2
Freyr Finnbogason <freyrbesti@gmail.com>
2025-09-21 12:15:15 +0000

    fix main.tf

--- infrastructure/concrete/main.tf
+++ infrastructure/concrete/main.tf
@@ -4,11 +4,13 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.resource_group.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                            = var.storage_account_name
+  resource_group_name             = azurerm_resource_group.resource_group.name
+  location                        = var.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  default_to_oauth_authentication = true
+  public_network_access_enabled   = false
   network_rules {
     default_action = "Deny"
     bypass         = ["AzureServices"]
